0072043ec9cdb978c7e77c7b4f006425bf031a3a
Bun-Boo <ios02052001@gmail.com>
2023-05-25 15:39:28 +0700

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,25 +2,12 @@ import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { IonNav } from '@ionic/react';
 
+import PageOne from './page-one'
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+  return <IonNav root={() => <PageOne />}></IonNav>
+  
 };
 
 export default Home;
